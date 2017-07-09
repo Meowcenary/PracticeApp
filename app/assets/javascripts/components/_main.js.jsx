003ce120f7a21cf76da5877dd0b260d81ec31231
Eric Newman <newmaneric5@gmail.com>
2017-07-08 23:00:43 -0500

    A bunch of stuff. Index and Create view separated. Working on early steps to refactoring.

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -3,10 +3,38 @@ var Main = React.createClass({
         console.log('Component mounted');
     },
 
+    getInitialState() {
+        return {
+                active_pane: 'characters_index'
+               }
+    },
+
+    activePane() {
+        if(this.state.active_pane === 'characters_index') {
+            return  <CharactersIndex />
+        } else if(this.state.active_pane === 'new_character_form') {
+            return <NewCharacter />
+        }
+    },
+
+    toggleNewCharacterFormActive() {
+        this.setState({
+            active_pane: 'new_character_form'
+        })
+    },
+
+    toggleCharactersIndexActive() {
+        this.setState({
+            active_pane: 'characters_index'
+        })
+    },
+
     render() {
         return  <div>
-                    <NewCharacter />
-                    <CharactersIndex />
+                    <button onClick={this.toggleNewCharacterFormActive}>Add Character</button>
+                    <button onClick={this.toggleCharactersIndexActive}>Character Index</button>
+                    <br/>
+                    {this.activePane()}
                 </div>
     }
 });
