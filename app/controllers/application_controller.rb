1e17ee187593ba178fdc1aa6117a75316c04ac44
Eric Newman <newmaneric5@gmail.com>
2017-09-22 22:07:33 -0500

    Small change, moved authentication from api base controller to application controller proper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   #returning null session b/c requesting json not html
-  protect_from_forgery with: :null_session, prepend: true
+  protect_from_forgery with: :exception, prepend: true
+  before_action :authenticate_user!
 end
